45b1a0467325421aaf085f4959493144b298d21a
Kea-Angel-Ntheledi <kealebogaangelntheledi@gmail.com>
2024-05-18 20:28:52 +0200

    Quick debug

--- book-detail.js
+++ book-detail.js
@@ -1,46 +1,43 @@
 class BookDetail extends HTMLElement {
-    constructor() {
-        super();
-        this.attachShadow({ mode: 'open' });
-    }
-}
+  constructor() {
+    super();
+    this.attachShadow({ mode: "open" });
+  }
 
-connectedCallback() {
-    const image = this.getAttribute('image');
-    const title = this.getAttribute('title');
-    const subtitle = this.getAttribute('subtitle');
-    const description = this.attribute('description');
-} 
+  connectedCallback() {
+    const image = this.getAttribute("image");
+    const title = this.getAttribute("title");
+    const subtitle = this.getAttribute("subtitle");
+    const description = this.getAttribute("description");
 
-this.shadowRoot.innerHTML = `
-      <style>
-        /* Styling for the component */
-        .detail-overlay {
-          display: flex;
-          flex-direction: column;
-          background: rgba(255, 255, 255, 0.9);
-          padding: 20px;
-          border-radius: 10px;
-        }
-        .detail-overlay img {
-          width: 100%;
-          height: auto;
-        }
-        .detail-overlay h2 {
-          margin: 10px 0;
-        }
-        .detail-overlay p {
-          color: #333;
-        }
-      </style>
-       <div class="detail-overlay">
+    this.shadowRoot.innerHTML = `
+        <style>
+          .detail-overlay {
+            display: flex;
+            flex-direction: column;
+            background: rgba(255, 255, 255, 0.9);
+            padding: 20px;
+            border-radius: 10px;
+          }
+          .detail-overlay img {
+            width: 100%;
+            height: auto;
+          }
+          .detail-overlay h2 {
+            margin: 10px 0;
+          }
+          .detail-overlay p {
+            color: #333;
+          }
+        </style>
+        <div class="detail-overlay">
           <img src="${image}" />
           <h2>${title}</h2>
           <h3>${subtitle}</h3>
           <p>${description}</p>
         </div>
       `;
-    }
   }
-  
-  customElements.define('book-detail', BookDetail);
+}
+
+customElements.define("book-detail", BookDetail);
