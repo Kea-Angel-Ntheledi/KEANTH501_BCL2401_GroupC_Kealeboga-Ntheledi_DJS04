2a27d7dc4185fabf294d0c2a4806d6a724990871
Kea-Angel-Ntheledi <kealebogaangelntheledi@gmail.com>
2024-05-18 20:03:59 +0200

    added code for when an element is added to the DOM

--- book-detail.js
+++ book-detail.js
@@ -4,3 +4,10 @@ class BookDetail extends HTMLElement {
         this.attachShadow({ mode: 'open' });
     }
 }
+
+connectedCallback() {
+    const image = this.getAttribute('image');
+    const title = this.getAttribute('title');
+    const subtitle = this.getAttribute('subtitle');
+    const description = this.attribute('description');
+}
