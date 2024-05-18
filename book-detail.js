1bc469d07955da0dfb79c429a5cb483838ddaa39
Kea-Angel-Ntheledi <kealebogaangelntheledi@gmail.com>
2024-05-18 20:11:04 +0200

    set the inner HTML of the shadow DOM, including styles and structure

--- book-detail.js
+++ book-detail.js
@@ -11,3 +11,24 @@ connectedCallback() {
     const subtitle = this.getAttribute('subtitle');
     const description = this.attribute('description');
 } 
+
+this.ShadowRoot.innerHTML = `
+<style>
+    .detail-overlay{
+        display: flex;
+        flex-direction: column;
+        background: rgba(255,255,255,0.9);
+        padding: 20px;
+        border-radius: 10px;
+    }
+    .detail-overlay img {
+        width: 100%;
+        height: auto;
+    }
+    .detail-overlay h2{
+        margin: 10px 0;
+    }
+    .detail-overlay p{
+        color: #333;
+    }
+    </style>
